b5f0f774909004b8dfa6227c9b44b922e76dd798
Jaico1 <jaimecoronado07@gmail.com>
2023-11-16 21:34:07 +0100

    creating geometry module

---
+++ Source/ModuleGeometryLoader.cpp
@@ -0,0 +1,2 @@
+#include "ModuleGeometryLoader.h"
+#include "tiny_glft.h"
---
+++ Source/ModuleGeometryLoader.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Module.h"
+class ModuleGeometryLoader :
+    public Module
+{
+public:
+
+	ModuleGeometryLoader();
+	~ModuleGeometryLoader();
+
+	bool Init();
+	update_status PreUpdate();
+	update_status Update();
+	update_status PostUpdate();
+
+	bool CleanUp();
+
+};
+
